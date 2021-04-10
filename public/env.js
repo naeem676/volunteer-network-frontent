73b56ca54799e97ea543d640d33d0b06c7433810
naeem uddin <ali.naeem939@gmail.com>
2021-04-10 18:02:55 +0600

    dotenv added

---
+++ public/env.js
@@ -0,0 +1,8 @@
+window.env = {
+  "API_KEY": "AIzaSyAe3LJk5ASFV__lo4fasCLXuEzl1IZPHJU",
+  "AUTH_DOMAIN": "volunteer-service-7e2cc.firebaseapp.com",
+  "PROJECT_ID": "volunteer-service-7e2cc",
+  "STORAGE_BUCKET": "volunteer-service-7e2cc.appspot.com",
+  "MESSAGING_SENDER_ID": "611434165367",
+  "APP_ID": "1:611434165367:web:25005217d528e1ed6ac272"
+};
--- src/components/login/FirebaseConfig.js
+++ src/components/login/FirebaseConfig.js
@@ -1,10 +1,13 @@
+import env from "react-dotenv";
 const firebaseConfig = {
-    apiKey: "AIzaSyAe3LJk5ASFV__lo4fasCLXuEzl1IZPHJU",
-    authDomain: "volunteer-service-7e2cc.firebaseapp.com",
-    projectId: "volunteer-service-7e2cc",
-    storageBucket: "volunteer-service-7e2cc.appspot.com",
-    messagingSenderId: "611434165367",
-    appId: "1:611434165367:web:25005217d528e1ed6ac272"
+    apiKey: `${env.API_KEY}`,
+    authDomain: `${env.AUTH_DOMAIN}`,
+    projectId: `${env.PROJECT_ID}`,
+    storageBucket: `${env.STORAGE_BUCKET}`,
+    messagingSenderId: `${env.MESSAGING_SENDER_ID}`,
+    appId: `${env.APP_ID}`
   };
 
   export default firebaseConfig;
+
+  
